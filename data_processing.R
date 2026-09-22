3d938c7e22de06d3445d2795c08359b028a972a0
Ayden Matocha <aydenmatocha@gmail.com>
2026-09-22 14:03:39 -0400

    initial dashboard

--- data_processing.R
+++ data_processing.R
@@ -11,74 +11,6 @@ off_epas <- c()
 def_epas <- c()
 sos <- c()
 
-for (i in teams) { # Get win%, point diff, off epa, and def epa
-  team_stats <- get_team_stats(i)
-  win_percentages <- c(win_percentages, team_stats[1])
-  point_diffs <- c(point_diffs, team_stats[2])
-  
-  team_epas <- get_team_epa(i)
-  off_epas <- c(off_epas, team_epas[1])
-  def_epas <- c(def_epas, team_epas[2])
-}
-
-initial_values <- data.frame( # Create df with the initial values
-  team = teams,
-  win_pct = win_percentages,
-  point_diff = point_diffs,
-  off_epa = off_epas,
-  def_epa = def_epas
-)
-
-# Calculate initial ratings
-min_pd <- min(initial_values$point_diff)
-max_pd <- max(initial_values$point_diff)
-pd_range <- max_pd - min_pd
-
-initial_ratings <- get_init_ratings(initial_values)
-
-# Calculate SOS
-for (i in teams) {
-  team_schedule <- get_team_standings(i)
-  opponents <- unique(team_schedule$opponent)
-  win_percentages_removed <- c()
-  point_diffs_removed <- c()
-  off_epas_removed <- c()
-  def_epas_removed <- c()
-  
-  for (j in opponents) { # Calculate average initial rating of opponents (not including stats against current team)
-    team_stats <- get_team_stats_removed(j, i)
-    win_percentages_removed <- c(win_percentages_removed, team_stats[1])
-    point_diffs_removed <- c(point_diffs_removed, team_stats[2])
-    
-    team_epas <- get_team_epa_removed(j, i)
-    off_epas_removed <- c(off_epas_removed, team_epas[1])
-    def_epas_removed <- c(def_epas_removed, team_epas[2])
-  }
-  
-  initial_values_removed <- data.frame( # Create df with the initial values
-    team = teams,
-    win_pct = win_percentages_removed,
-    point_diff = point_diffs_removed,
-    off_epa = off_epas_removed,
-    def_epa = def_epas_removed
-  )
-  
-  initial_ratings_removed <- get_init_ratings(initial_values_removed)
-  sos <- c(sos, mean(initial_ratings_removed$total))
-}
-
-min_sos <- min(sos)
-max_sos <- max(sos)
-sos_range <- max_sos - min_sos
-
-sos_ratings <- get_sos_rating(sos)
-
-final_ratings <- initial_ratings %>% select(-total)
-final_ratings$sos <- sos_ratings
-final_ratings <- final_ratings %>%
-  mutate(total = win_pct + point_diff + off_epa + def_epa + sos) %>%
-  arrange(-total)
-
 # Helper functions
 
 get_team_standings <- function(team_input) {
@@ -146,6 +78,32 @@ get_team_epa_removed <- function(team_input, team_removed) {
   return(team_epas)
 }
 
+
+# Begin calculations
+
+for (i in teams) { # Get win%, point diff, off epa, and def epa
+  team_stats <- get_team_stats(i)
+  win_percentages <- c(win_percentages, team_stats[1])
+  point_diffs <- c(point_diffs, team_stats[2])
+  
+  team_epas <- get_team_epa(i)
+  off_epas <- c(off_epas, team_epas[1])
+  def_epas <- c(def_epas, team_epas[2])
+}
+
+initial_values <- data.frame( # Create df with the initial values
+  team = teams,
+  win_pct = win_percentages,
+  point_diff = point_diffs,
+  off_epa = off_epas,
+  def_epa = def_epas
+)
+
+# Calculate initial ratings
+min_pd <- min(initial_values$point_diff)
+max_pd <- max(initial_values$point_diff)
+pd_range <- max_pd - min_pd
+
 get_init_ratings <- function(input_df) {
   output <- input_df %>% mutate(
     win_pct = round(win_pct * 2, 2), # min: 0%, mean: 50%, max: 100%
@@ -158,8 +116,55 @@ get_init_ratings <- function(input_df) {
   return(output)
 }
 
+initial_ratings <- get_init_ratings(initial_values)
+
+# Calculate SOS
+for (i in teams) {
+  team_schedule <- get_team_standings(i)
+  opponents <- unique(team_schedule$opponent)
+  win_percentages_removed <- c()
+  point_diffs_removed <- c()
+  off_epas_removed <- c()
+  def_epas_removed <- c()
+  
+  for (j in opponents) { # Calculate average initial rating of opponents (not including stats against current team)
+    team_stats <- get_team_stats_removed(j, i)
+    win_percentages_removed <- c(win_percentages_removed, team_stats[1])
+    point_diffs_removed <- c(point_diffs_removed, team_stats[2])
+    
+    team_epas <- get_team_epa_removed(j, i)
+    off_epas_removed <- c(off_epas_removed, team_epas[1])
+    def_epas_removed <- c(def_epas_removed, team_epas[2])
+  }
+  
+  initial_values_removed <- data.frame( # Create df with the initial values
+    team = teams,
+    win_pct = win_percentages_removed,
+    point_diff = point_diffs_removed,
+    off_epa = off_epas_removed,
+    def_epa = def_epas_removed
+  )
+  
+  initial_ratings_removed <- get_init_ratings(initial_values_removed)
+  sos <- c(sos, mean(initial_ratings_removed$total))
+}
+
+min_sos <- min(sos)
+max_sos <- max(sos)
+sos_range <- max_sos - min_sos
+
 get_sos_rating <- function(input_vector) {
   input_vector <- round((((input_vector - min_sos) / sos_range) * 2), 2)
   
   return(input_vector)
 }
+
+sos_ratings <- get_sos_rating(sos)
+
+final_ratings <- initial_ratings %>% select(-total)
+final_ratings$sos <- sos_ratings
+final_ratings <- final_ratings %>%
+  mutate(total = win_pct + point_diff + off_epa + def_epa + sos) %>%
+  arrange(-total)
+
+
